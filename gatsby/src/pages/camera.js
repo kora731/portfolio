1cf1ea1b2c9e903f2eb85167f694e5ec386a0f55
i066723 <li.ding@sap.com>
2019-07-09 09:58:40 -0700

    fix

--- gatsby/src/pages/camera.js
+++ gatsby/src/pages/camera.js
@@ -122,112 +122,57 @@ const renderSection= (key) => {
         case "Brief":
             return <Brief attributes={data.briefAttribute}/>;
 
-        case "VR Video Pipeline":
+        case "Capture recordings":
             return <>
-                <span>Current With Jaunt Cloud Services(JCS), users can apply cloud rendering to stitch 24 footage together, and transcode them into different formats. The cloud services help prepare video media for distribution on all VR platforms instead of managing files one by one. Along with the rendering, JCS also provides the service modules of Publishing and Analytics to help creators distribute videos and track the audience engagement.</span>
+                <span>Jaunt flagship product Jaunt One camera is paired with the camera controller application to access a wide variety of features. While connecting to the camera, users can adjust the camera exposure, capture preview and even get real time views. Jaunt media manager is the gateway tool to transfer recordings between VR camera, cloud services and user local data. It organizes 24 camera footage into formats ready for ingestion, to upload and download from the cloud.</span>
                 <p/>
 
                 <img alt="" className="img-responsive" src={System}/>
 
-                <h4 className="text-bold mt-40">Targeted Users</h4>
-                <span>JCS serves people who are working in VR movie production, including Editors, Producers and Jaunt internal people to trouble shoot. Utilizing VR as a new medium and territory to do storytelling and deliver cinematic VR experiences, JCS users try to focus on the creative side and make most of rendering platform and services.</span>
-                <p/>
-                <img alt="" className="img-responsive" src={User}/>
-
-                <h4 className="sectionTitle text-bold mt-40">Experience Map of today’s production</h4>
-                <span>I started the project by interviewing in-house production team. Together we mapped out the full journey of creating videos, so that we could have a holistic view to prioritize breakdown points.</span>
+                <h4 className="text-bold mt-40">Camera Controller App Pain Points</h4>
+                <span>Overloaded with lots of features and tangled statuses, the users had a hard time understanding all the modes and controls. Also the camera has 24 modules which may need to manual exposure adjustment of specific modules. How to design the viewport layout to support both global and individual camera view?</span>
                 <p/>
 
-                <img alt="" className="img-responsive" src={ExMap}/>
-                <h4 className="text-bold mt-40">Key problems of production pipelinet</h4>
-                <span>1. Media centric UI rather than project based. Users have a hard time managing media across projects and the naming of media is hard to manage and often lead to errors.</span>
-                <p/>
-                <span>2. No transparency on the rendering status, and upload/download sessions are complicated.</span>
-                <p/>
-                <span>3. How to onboard new users who don’t have video editing or production experiences?</span>
+                <h4 className="sectionTitle text-bold mt-40">Media Manager App Pain Points</h4>
+                <span>VR video data size is very large at 50+ GB level sheer size with 4+ formats of stitched results. Long download time and internet connection stability are also challenging to manage the media in a secure and stable way.</span>
                 <p/>
             </>;
 
-        case "Production Design Challenge":
+        case "Design Challenge":
             return <>
                 <ContentHighlight attributes={data.highlight1}/>
-                <h4 className="text-bold mt-40">Design Process</h4>
-                <span>I’m the only product designer working with a team of four engineers and two product managers. We’re moving fast on shipping incremental features. Meanwhile PM and I are working on the next version of the JCS platform to help prioritize features.</span>
+                <h4 className="text-bold mt-40">Experience Goals</h4>
+                <span>Alleviate the user burden - redesign configuration flow to make it much easier and automate unnecessary steps</span>
                 <p/>
-                <span>When there is request or complaint from end users, we started by talking to our internal production team to understand the problem better. If we have enough confidence, then the team will sketch out various solutions and discuss their feasibility.</span>
+                <span>Transparent system status - monitor the system changes, visualize the progress up-front for the user</span>
                 <p/>
-                <span>I’ll lead the next step to visualize top two solutions with wireframes and flow charts. Then our team will share these two low-fi solutions with external users for feedback. Once we confirmed on the use case and solution, technical lead will start building the structure while I’m working on the UI delivery to ensure usability and quality of the solution design.</span>
+                <span>Balance advanced powerful settings with simplicity to support various user needs</span>
+                <p/>
+                <span>Fluid and scalable UI to support newly added features and long term vision</span>
                 <p/>
             </>;
 
-        case "SOLUTION FRAMEWORK":
+        case "Solution framework":
             return <>
                 
             </>;
 
-        case "Redesign Service Structure":
+        case "Media Manager":
             return <>
-                <img alt="" className="img-responsive" src={Service}/>
-                <h4 className="text-bold mt-40">Wireframe of project oriented content management flow</h4>
-                <img alt="" className="img-responsive" src={Wire}/>
+
             </>;
 
-        case "Improve video editing experience":
+        case "Camera Controller":
             return <>
-                <span>I lead the design of improving timeline editor where the users can quickly trim the video beginning and end points to reduce rendering cost. Below is the interaction diagram of time scrub and player handle to propose a simplified editing and saving process. 
-                <p/>Once the feature is released to our users, they shared back that editing and saving clips have been much faster and they like the ability to easily modify the scrub and time handle.
-                </span>
-                <div class="row">
-                    <img alt="" className="img-responsive col-lg-6 col-sm-12" src={Timeline1}/>
-                    <img alt="" className="img-responsive col-lg-6 col-sm-12" src={Timeline1}/>
-                </div>
-                <img alt="" className="img-responsive" src={Timeline3}/>
-            </>;
-
-        case "User onboarding flow":
-            return <>
-                
-                <img alt="" className="img-responsive" src={Tour}/>
-            </>;
-
-
-        case "Publishing and analytics":
-                return <>
-                     <img alt="" className="img-responsive" src={Sketch}/>
-                     <img alt="" className="img-responsive" src={Pub}/>
-                </>;
-            
-        case "Living style guide":
-            return <>
-            <span>While I were producing more pages, the team feel we need a redesign of the whole product UI. 
-            To start I decided to catalog current design tokens and components used, as well as page layout. 
-            In addition, there is a growing request for dark themed UI because most video production software are in dark mode already. 
-            Dark theme is easier on the eyes while presenting rich media data. I lead the style guide design outside project work, and document the final style 
-            guide online. It leads to our frontend engineers complete the dark themed UI within two weeks. </span>
-
-            <img alt="" className="img-responsive" src={StyleGuide}/>
-            <p className="text-center mt-20">
-                <a className="btn btn-3d bt-lg" href="https://codepen.io/kora731/pen/dNdZgK" target="_blank">View Style Guide Online</a>
-            </p>
-            </>;
-
-        case "Visualize the blackbox":
-            return <>
-                <span>Having a super talented technical artist in the team (Rory), I invited him to collaborate on a hackathon project to visualize how the video were brought into final production ready. The robot metaphor helps onboard not only internal team but also external new users to understand the whole video pipeline.
-                    </span>
-
-             <img alt="" className="img-responsive" src={Robot}/>
-
 
             </>;
 
         case "Takeaway":
             return <>
-               <span>Design solution needs to balance between future long-term goal and current iterations, how to handle user feedback and understand the insights instead of only following features asked for. To come up with effective solutions, designers need to learn about and prepare for the big picture and understand the why better. For example, a large part of the system today is built upon request, but what turned out happening is that the features built are not used by the end user. 
-                 <p/>
-                When users ask for certain features, should we really follow the comments? or we need to rethink through the deeper reason and flow, why they are like this today. Remind me of Ford’s faster horse theory. Designers need to consolidate the insights, collaboratively define the vision we’re heading towards.
-               </span>
-               <ImageGallery items={data.images}/>;
+                <span>Fast and slow:  During the first week of joining Jaunt, I was able to design the download module right away. However, in the following months, I start learning more about how users actually interact with the Media Manager, only to discover the initial download dialog is not addressing the key pain points. Then I started slow iteration of the download dialog, to craft each interaction to support the user tasks better. The easiest and most accessible, right at hand, solution always poses high risk to actually solving the problem. Because it mostly overlooks the factors and edge cases, and that's where UX matters most. It has to be thought through with all use cases, and test against all possible routes.</span>
+                <p/>
+                <span>Camera is very prevalent in our daily life. But the adjustable controls vary a lot between mobile and professional cameras. Optimizing the UI layout alone doesn’t help address the workflow problem. Also with so much customization involved, how to uncover the mental model is also critical.</span>
+                <p/>
             </>;
 
         default:
