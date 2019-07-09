e9e826f2d3ac4251677db15404d07ce39816fa81
Surong Ruan <surong731@gmail.com>
2019-07-09 11:13:49 -0700

    fix images for camera

--- gatsby/src/pages/camera.js
+++ gatsby/src/pages/camera.js
@@ -8,19 +8,27 @@ import Template from "../components/template";
 
 /* Image Assets */
 import Cover from "../images/cam/cam-cover.png"
-import System from "../images/jcs/jcs-system.png"
-import User from "../images/jcs/jcs-persona.jpg"
-import ExMap from "../images/jcs/jaunt-expmap.png"
-import Service from "../images/jcs/jcs-services.png"
-import Wire from "../images/jcs/jcs-wire.png"
-import Tour from "../images/jcs/jcs-onboard.png"
-import Sketch from "../images/jcs/jcs-sketch.png"
-import Pub from "../images/jcs/jcs-pub.png"
-import StyleGuide from "../images/jcs/jcs-styleguide.png"
-import Robot from "../images/jcs/jcs_robot.jpg"
-import Timeline1 from "../images/jcs/P2 Recording Create Dis.png"
-import Timeline2 from "../images/jcs/P2 Timeline Flow.png"
-import Timeline3 from "../images/jcs/P2 IxD.png"
+import System from "../images/cam/jaunt-pipeline.png"
+import IA from "../images/cam/jaunt-ia.png"
+import DozerIXD from "../images/cam/dozer-ixd.png"
+import DozerUI from "../images/cam/dozer-ui.png"
+import Sync from "../images/cam/dozer-syncmedia.png"
+import Download from "../images/cam/dozer-download.png"
+import TriV1A from "../images/cam/tri-v1-1.png"
+import TriV1B from "../images/cam/tri-v1-2.png"
+import TriV2A from "../images/cam/tri-v2-1.png"
+import TriV2B from "../images/cam/tri-v2-2.png"
+import TriV3A from "../images/cam/tri-v3-1.png"
+import TriV3B from "../images/cam/tri-v3-2.png"
+import TriStep1 from "../images/cam/tri-step1.png"
+import TriStep2 from "../images/cam/tri-step2.png"
+import TriStep3 from "../images/cam/tri-step3.png"
+import TriStep4 from "../images/cam/tri-step4.png"
+import TriStep5 from "../images/cam/tri-step5.png"
+import TriStep6 from "../images/cam/tri-step6.png"
+import TriStep7 from "../images/cam/tri-step7.png"
+import TriStep8 from "../images/cam/tri-step8.png"
+import TriStep9 from "../images/cam/tri-step9.png"
 
 const data = {
     name: "Pipeline",
@@ -29,7 +37,7 @@ const data = {
         href: "/project/pipeline/index.html"
     },
     next: {
-        label: "First Experience",
+        label: "Autodesk",
         href: "/project/fe/index.html"
     },
     title: "Jaunt Desktop App: Camera Controller and Media Manager",
@@ -60,7 +68,7 @@ const data = {
         },
         {
             label: "Outcome",
-            value: "Very well received new controller UI and media manager releases. Timelapse and live feed features are top rated functions by Jaunt camera users"
+            value: "Very well received app releases. Timelapse and live feed features are top rated functions by Jaunt camera users"
         }
     ],
     aTitle: [
@@ -89,28 +97,53 @@ const data = {
             key:  "Solution framework"
         },
         {
+            label: "Outcome",
+            key:  "Outcome"
+        },
+        {
             label: "Takeaway",
             key:  "Takeaway"
         },
       
     ],
     images: [
-        // {
-        //     original: Specs1,
-        //     thumbnail: Specs1,
-        // },
-        // {
-        //     original: Specs2,
-        //     thumbnail: Specs2,
-        // },
-        // {
-        //     original: Specs3,
-        //     thumbnail: Specs3,
-        // },
-        // {
-        //     original: Specs4,
-        //     thumbnail: Specs4,
-        // }
+        {
+            original: TriStep1,
+            thumbnail: TriStep1,
+        },
+        {
+            original: TriStep2,
+            thumbnail: TriStep2,
+        },
+        {
+            original: TriStep3,
+            thumbnail: TriStep3,
+        },
+        {
+            original: TriStep4,
+            thumbnail: TriStep4,
+        },
+        {
+            original: TriStep5,
+            thumbnail: TriStep5,
+        },
+        {
+            original: TriStep6,
+            thumbnail: TriStep6,
+        },
+        {
+            original: TriStep7,
+            thumbnail: TriStep7,
+        },
+        {
+            original: TriStep8,
+            thumbnail: TriStep8,
+        },
+        {
+            original: TriStep9,
+            thumbnail: TriStep9,
+        },
+      
     ]    
 };
 
@@ -138,7 +171,7 @@ const renderSection= (key) => {
                 <p/>
             </>;
 
-        case "Design Challenge":
+        case "Design challenge":
             return <>
                 <ContentHighlight attributes={data.highlight1}/>
                 <h4 className="text-bold mt-40">Experience Goals</h4>
@@ -154,19 +187,78 @@ const renderSection= (key) => {
 
         case "Solution framework":
             return <>
-                
+                <img alt="" className="img-responsive" src={IA}/>
             </>;
 
-        case "Media Manager":
+        case "Media manager":
             return <>
+                <h4 className="text-bold mt-40">Download media workflow and UI</h4>
+                <div className="row">
+                    <img alt="" className="img-responsive col-lg-8" src={DozerIXD}/>
+                    <img alt="" className="img-responsive col-lg-4" src={DozerUI}/> 
+                </div>
+
+                <h4 className="text-bold mt-40">Cloud Sync Model</h4>
+                <img alt="" className="img-responsive" src={Sync}/> 
+
+                <h4 className="text-bold mt-40">Final product focusing on download feature</h4>
+                <span>Although synchronization model between local and cloud is desirable, it poses huge technical challenges for the team. Given the limited engineering resources, we re-define the download feature scope to focus on patch download. After talking to more editors and data wranglers, we learnt that users mostly will gather the same type of media during each session. With default setup to most frequent use cases, the user is still able to customize to their special case needs.
+                    </span>
+                    <img alt="" className="img-responsive" src={Download}/> 
 
             </>;
 
-        case "Camera Controller":
+        case "Camera controller":
             return <>
+            <h4 className="text-bold mt-40">Experience Framework</h4>
+            <p>How to provide a more fluid and scalable UI with new added features
+                <br/>- to adopt a similar mental model to DSLR camera operation: switch between commands and actions, instead of showing all buttons and UI at once, prioritize and group functions.</p>
+            <p>How to make widgets more accessible and efficient
+                <br/>- to lower the learning bar to understand the UI, focus on user tasks to simplify the flow. Learn from mobile camera apps, simple WYSIWYG model while still powerful with advanced settings</p>
 
+            <h4 className="text-bold mt-40">IA Structure</h4>
+            <span>Modes:  Record or Time-lapse,  Capture Preview and Video Feed 
+            <p/>Settings: The camera has Setting Profile, difference between auto and manual modes 
+            <p/>Viewport: View of All / Individual Camera Modules: naive stitch, list, side by side
+            </span>
+
+            <h4 className="text-bold mt-40">Design Iterations</h4>
+            
+            <span>Iteration 1:
+                <p/>My first iteration is to design the button group, which helps prioritize functions. But the introduction of modes is making design more complicated. The coupling relationship makes users more confused with the view and action dependency. Also the location of the customization view is not scalable, and taking up the valuable screen space. 
+                </span>
+                <div className="row mt-20 mb-20">
+                    <img alt="" className="img-responsive col-lg-6" src={TriV1A}/>
+                    <img alt="" className="img-responsive col-lg-6" src={TriV1B}/> 
+                </div>
+
+                <span> Iteration 2:<p/>
+               For the next iteration, I tried to have the left sidebar showing the default parameters while expanding to edit: powerful but also too complicated, not mapping to camera controller mental model. Live feed as a separate way to streaming views, is designed with more visual emphasis to encourage user interaction to try it out.
+
+                </span>
+                <div className="row mt-20 mb-20">
+                    <img alt="" className="img-responsive col-lg-6" src={TriV2A}/>
+                    <img alt="" className="img-responsive col-lg-6" src={TriV2B}/> 
+                </div>
+
+                <span>Iteration 3:<p/>
+                While research and keep trying out different layout, it's critical to remember the base principle of balancing simplicity with being powerful. Then I introduce the toggle control of camera settings to allow for hide / show components.
+                </span>
+                <div className="row mt-20 mb-20">
+                    <img alt="" className="img-responsive col-lg-6" src={TriV3A}/>
+                    <img alt="" className="img-responsive col-lg-6" src={TriV3B}/> 
+                </div>
             </>;
+        case "Outcome":
+            return <>
+            
+            
+            <span>
+            When I shared the proposal via InVision prototype with creators remotely, they are very excited about the upcoming timelapse new feature. I worked tightly with the app engineer to prepare all the assets and UI specs, and got the app released in May 2017. It was well received among creators using Jaunt camera.
+            </span>
+            <ImageGallery items={data.images}/>
 
+        </>;
         case "Takeaway":
             return <>
                 <span>Fast and slow:  During the first week of joining Jaunt, I was able to design the download module right away. However, in the following months, I start learning more about how users actually interact with the Media Manager, only to discover the initial download dialog is not addressing the key pain points. Then I started slow iteration of the download dialog, to craft each interaction to support the user tasks better. The easiest and most accessible, right at hand, solution always poses high risk to actually solving the problem. Because it mostly overlooks the factors and edge cases, and that's where UX matters most. It has to be thought through with all use cases, and test against all possible routes.</span>
